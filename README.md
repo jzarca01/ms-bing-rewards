d6ca66fee3c4bcdc1d2cb10658929a52581d1b25
Jeremie Zarca <jeremiezarca@MacBook-Pro-de-Jeremie.local>
2017-11-13 16:22:26 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# ms-bing-rewards
+# MS Bing Rewards
+
+Automates your Bing searches
+
+## Individual run
+
+```
+node rewards.js -e youraccount@email.com -p youraccount@password
+```
+
+Checks your Rewards level and does
+- 10 searches if you are level 1
+- 30 desktop searches and 20 mobile searches if you are level 2
+
+
+## Bulk run
+
+If you want to bulk run multiple accounts
+
+```
+node rewards.js -l youaccountlist.txt -p youruniquepassword
+```
---
+++ rewards.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+const program = require('commander');
+const clear = require('clear');
+const fs = require('fs');
+
+
+const RewardsApi = require('./lib/rewards-api');
+const rewardsApi = new RewardsApi();
+
+let ERRORS = [];
+
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array)
+    }
+  }
+
+program
+  .version('1.0')
+  .option('-e, --email [value]', 'Specify your email address')
+  .option('-l --list [file]', 'Specify a file')
+  .option('-p, --password [value]', 'Specify the associated password')
+  .parse(process.argv);
+
+if((!program.email || !program.list) && !program.password ) {
+    ERRORS.push("Please provide valid email or list and password");
+}
+
+if(ERRORS.length > 0) {
+    clear();
+    console.log(ERRORS.join("\n"));
+    return;    
+}
+
+if(program.list) {
+    fs.readFile(program.list, function(err, data) {
+        if(err) throw err;
+
+        const makeRequest = async(account) => {
+            await rewardsApi.getRewards(account, program.password);
+        }
+
+        const accounts = fs.readFileSync(program.list).toString().split("\n");
+        
+        asyncForEach(accounts, async(account) => {
+            await makeRequest(account);
+        });
+    });
+}
+
+else {
+    rewardsApi.getRewards(program.email, program.password)
+    .catch(error => {
+        console.log("An error occured");
+        return;
+    });
+}
